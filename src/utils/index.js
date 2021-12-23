ac84f31e89e0542962805dffb15f68811c8fd804
xingyifan <xingyifan10@qq.com>
2021-12-23 21:34:23 +0800

    feat: 添加useMount 和 useDebounce 两个hook

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (object) => {
@@ -9,4 +11,25 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+// 进入页面只请求一次的hook
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// 防抖hook，对input的传入参数进行debounce
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次value变化后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行，消除上一个
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };
